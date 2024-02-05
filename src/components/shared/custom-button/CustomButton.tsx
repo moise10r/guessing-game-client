ae31a864a103825a7c412d3066df2ca4af6e4376
moise10r <moiserushanika2006@gmail.com>
2024-02-05 17:48:36 +0400

    Add a resuable custom button component

---
+++ src/components/shared/custom-button/CustomButton.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
+
+type ButtonType = 'button' | 'submit' | 'reset';
+
+interface CustomButtonProps {
+  children?: React.ReactNode | React.ReactNode[]
+  variant?: 'solid' | 'bordered' | 'light' | 'flat' | 'faded' | 'shadow' | 'ghost';
+  color?: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger';
+  radius?: 'none' | 'sm' | 'md' | 'lg' | 'full';
+  isDisabled?: boolean;
+  type?: ButtonType;
+  text: string;
+  href?: string;
+  className?: string;
+  textStyles?: string;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  fullWidth?: boolean;
+  handleClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export const CustomButton: React.FC<CustomButtonProps> = (props) => {
+  const {
+    variant, color, radius, isDisabled, type, text, href, className, fullWidth, handleClick
+  } = props;
+
+
+  return (
+    <Button
+      variant={variant || "solid"}
+      color={color || "default"}
+      radius={radius || "md"}
+      isDisabled={isDisabled}
+      type={type || "button"}
+      startContent={props.startIcon}
+      endContent={props.endIcon}
+      className={`min-w-[30px] ${className}`}
+      fullWidth={fullWidth}
+      onClick={handleClick}
+      {...(href && { as: Link, href })}
+    >
+      {text}
+    </Button>
+  );
+};
